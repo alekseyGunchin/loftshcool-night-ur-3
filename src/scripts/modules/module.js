78df15bf2ed84ef5f60b479a91343c5c310993cb
alekseyGunchin <a.s.gunchin@gmail.com>
2018-03-14 12:51:16 +0300

    add enterance section

--- src/scripts/modules/module.js
+++ src/scripts/modules/module.js
@@ -1,1 +1,12 @@
-console.log('hello from module');
+var btns = document.getElementsByClassName('btn-entrance__link');
+var log = document.getElementsByClassName('form-entrance__login');
+var welc = document.getElementsByClassName('form-entrance__welcome');
+var main = document.getElementsByClassName('btn-form__link-login');
+btns[0].onclick = function() {
+    log[0].classList.add("form-entrance_active");
+    welc[0].classList.remove("form-entrance_active");
+}
+main[1].onclick = function() {
+    log[0].classList.remove("form-entrance_active");
+    welc[0].classList.add("form-entrance_active");
+}
